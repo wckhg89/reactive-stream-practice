1d78d52a77dc69e4ed267cb7a689a2273f9ccf5e
kanghonggu <wckhg89@gmail.com>
2017-12-15 18:32:26 +0900

    git init

---
+++ src/test/java/ObserableTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by guppy.kang on 2017. 12. 15.
+ * email : guppy.kang@kakaocorp.com
+ */
+
+
+public class ObserableTest {
+
+    @Test
+    public void ObserableTest () {
+
+    }
+}
---
+++ src/test/java/ReactiveStreamTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by guppy.kang on 2017. 12. 15.
+ * email : guppy.kang@kakaocorp.com
+ */
+public class ReactiveStreamTest {
+
+    @Test
+    public void reactiveStreamTest () {
+
+    }
+
+}
