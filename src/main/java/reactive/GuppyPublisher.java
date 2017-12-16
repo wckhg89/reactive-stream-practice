be564cb55843818c663ec12e4e53bce8492ce5b3
guppy(강홍구) <guppy.kang@kakaocorp.com>
2017-12-16 15:54:31 +0900

    reactive stream 예제 코드 작성중.

---
+++ src/main/java/reactive/GuppyPublisher.java
@@ -0,0 +1,17 @@
+package reactive;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+/**
+ * Created by guppy.kang on 2017. 12. 16.
+ * email : guppy.kang@kakaocorp.com
+ */
+public class GuppyPublisher implements Publisher {
+
+    // Obserable 의 addObserver 와 같은 역할
+   @Override
+    public void subscribe(Subscriber s) {
+
+    }
+}
---
+++ src/main/java/reactive/ImazineSubscriber.java
@@ -0,0 +1,40 @@
+package reactive;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+/**
+ * Created by guppy.kang on 2017. 12. 16.
+ * email : guppy.kang@kakaocorp.com
+ */
+public class ImazineSubscriber implements Subscriber {
+
+
+    // onSubscribe는 최초 호출되는 메소드입니다.
+    // Subscriber를 사용할때에는 무조건 처음에 호출해야 하는 내용이 스펙에 등록되어있습니다.
+    @Override
+    public void onSubscribe(Subscription s) {
+
+    }
+
+
+    // Java Util Observer 의 update 와 같은 역할을 한다.
+    @Override
+    public void onNext(Object o) {
+
+    }
+
+
+    // 에러가 있으면 onError 에서 처리
+    @Override
+    public void onError(Throwable t) {
+
+    }
+
+
+    // 무사히 처리가 완료되면 onComplete 에서 처리
+    @Override
+    public void onComplete() {
+
+    }
+}
